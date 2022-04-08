47c80e7f29f50aa0094131924140cf255d4a1249
Cliford Guillaume <44070488+Clifordguillaume@users.noreply.github.com>
2022-04-08 12:01:01 -0700

    Initial Commit

---
+++ MapReduce_Old/MapReduce/Reduce.h
@@ -0,0 +1,26 @@
+// ===============================================================================
+// CSE 687 MapReduce Project
+// 
+// Reduce.h
+// 
+// Author: Elizaneth and Cliford
+// 
+// Description: 
+//              
+// 
+// Notes:
+// 
+// ===============================================================================
+#ifndef REDUCE_H
+#define REDUCE_H
+
+class Reduce
+{
+public:
+	Reduce();
+	~Reduce();
+
+private:
+
+};
+#endif /* REDUCE_H */
